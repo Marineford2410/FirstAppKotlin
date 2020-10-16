4aef97d39d1443d7fd435a49bf7c2cada662f143
Marineford2410 <marconava2410@gmail.com>
2020-10-15 21:05:07 -0500

    Agrgando botones y numeros random

--- app/src/main/java/com/unam/fesar/happybirthday/MainActivity.kt
+++ app/src/main/java/com/unam/fesar/happybirthday/MainActivity.kt
@@ -2,10 +2,41 @@ package com.unam.fesar.happybirthday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
+/**
+ * This activity allowa the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            // val toast = Toast.makeText(this, "Dice Roll", Toast.LENGTH_SHORT)
+            // toast.show()
+
+            // val resultTextView: TextView = findViewById(R.id.textView3)
+            // resultTextView.text = "6"
+
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView3)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
