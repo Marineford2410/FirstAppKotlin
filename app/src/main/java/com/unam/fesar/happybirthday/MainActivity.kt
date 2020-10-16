4b868f7456df3ee547f23f293324171a40f6e1fa
Marineford2410 <marconava2410@gmail.com>
2020-10-16 16:47:58 -0500

    Cambiando la manera en la que se accede a las Views

--- app/src/main/java/com/unam/fesar/happybirthday/MainActivity.kt
+++ app/src/main/java/com/unam/fesar/happybirthday/MainActivity.kt
@@ -2,10 +2,17 @@ package com.unam.fesar.happybirthday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.unam.fesar.happybirthday.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+
     }
 }
